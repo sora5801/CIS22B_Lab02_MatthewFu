a2075686322eda3f417c39fc033bec4f71228428
Matthew Fu <sora5801fu@aol.com>
2018-10-17 17:14:05 -0700

    Needs work

--- CIS22B_Lab02_MatthewFu.cpp
+++ CIS22B_Lab02_MatthewFu.cpp
@@ -4,16 +4,34 @@
 using namespace std;
 
 int stringLength(char* given_string);
-void stringNCopy();
-void stringNAdd();
+void stringNCopy(char str1[], char str2[], int num);
+void stringNAdd(char str3[], char str4[], int num2);
 void stringNCompare();
 
 
 
 int main() {
+   cout << "We have 4 string functions to test.\n\n\n.";
+   cout << "Input L if you want to test out the stringLength function, C if you want to test out stringNCopy" <<
+      "A if you want to test out the stringNAdd function, and M if you want to test out the stringNCompare function" << endl;
+   char again;
+   do {
+      char userInput;
+      if (userInput == 'L') {}
+      else if (userInput == 'C') {}
+      else if (userInput == 'A') {}
+      else if (userInput == 'M') {}
+      else {
+         cout << "Invalid Input \n";
+      }
 
+      cout << "Would you like to run the program again? (Y/N): " << endl;
+      cin >> again;
+   } while (again == 'y' || again == 'Y');
    return 0;
 }
+
+
 
 int stringLength(char* given_string)
 {
@@ -25,4 +43,12 @@ int stringLength(char* given_string)
    }
 
    return length;
+}
+
+void stringNCopy(char str1[], char str2[], int num) {
+
+}
+
+void stringNAdd(char str3[], char str4[], int num2) {
+
 }
